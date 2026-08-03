031703b3710aec5fe75538fe6f0d0121c086c939
EnjoyHairSpa <enjoyparrucchier@proton.me>
2026-08-03 18:02:43 +0200

    allinato config
    il file config non lo vedeva in quanto dentro gitgnore e quindi nn lo caricava la chiave dentro config cmq è publica quindi niente panico

---
+++ config.js
@@ -0,0 +1,4 @@
+// config.js
+const SUPABASE_URL = 'https://ashctxmmjrjgmakuzpjy.supabase.co'; 
+const SUPABASE_KEY = 'sb_publishable_eSsDyQAkrJZ_kiKnY27Idw_Fn6uQt2t'; 
+const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
