0d65504cfc19df8be2d3e648c3c853e02fcc1f39
EnjoyHairSpa <enjoyparrucchier@proton.me>
2026-08-21 01:36:28 +0200

    sisitemato il bottone percorso nella navbar
    inserito il percorso nella button nav sotto che prima portava a vedere i percosrsi acquistati invece di crearli

--- bottom-nav.js
+++ bottom-nav.js
@@ -4,7 +4,7 @@
 (function () {
 
   const NAV_ITEMS = [
-    { label: "Il Percorso", icon: "✨", href: "il-mio-percorso.html" },
+    { label: "Il Percorso", icon: "✨", href: "percorsi.html" },
     { label: "Prenota",     icon: "➕", href: "prenotazione.html" },
     { label: "Home",        icon: "🏠", href: "area-riservata.html" }
   ];
